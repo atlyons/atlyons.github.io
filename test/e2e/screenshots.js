ff9a9f53a19f7a0b79dde55e8814ec2a0ee21e7e
Austin Lyons <austinlyons@Austins-MacBook-Pro.local>
2017-04-27 12:33:42 -0400

    screenshots

--- test/e2e/screenshots.js
+++ test/e2e/screenshots.js
@@ -15,47 +15,56 @@ module.exports = {
 
             // Navigate to Popcrush.com
             .url(home)
+            .waitForElementVisible('body', 100)
             .assert.urlEquals(home)
             .saveScreenshot('screenshots/popcrush/smokeTest/home.png')
 
             // Navigate to NEWS
             .url(news)
+            .waitForElementVisible('body', 100)
             .assert.urlEquals(news)
             .saveScreenshot('screenshots/popcrush/smokeTest/news.png')
 
             // Navigate to MUSIC
             .url(music)
+            .waitForElementVisible('body', 100)
             .assert.urlEquals(music)
             .saveScreenshot('screenshots/popcrush/smokeTest/music.png')
 
             // Navigate to MOVIES
             .url(movies)
+            .waitForElementVisible('body', 500)
             .assert.urlEquals(movies)
             .saveScreenshot('screenshots/popcrush/smokeTest/movies.png')
 
             // Navigate to TV
             .url(tv)
+            .waitForElementVisible('body', 500)
             .assert.urlEquals(tv)
             .saveScreenshot('screenshots/popcrush/smokeTest/tv.png')
 
             // Navigate to POLLS
             .url(polls)
+            .waitForElementVisible('body', 500)
             .assert.urlEquals(polls)
             .saveScreenshot('screenshots/popcrush/smokeTest/polls.png')
 
             // Navigate to PHOTOS
             .url(photos)
+            .waitForElementVisible('body', 500)
             .assert.urlEquals(photos)
             .saveScreenshot('screenshots/popcrush/smokeTest/photos.png')
 
             // Navigate to VIDEOS
             .url(videos)
+            .waitForElementVisible('body', 500)
             .assert.urlEquals(videos)
             .saveScreenshot('screenshots/popcrush/smokeTest/videos.png')
  
 
             // Navigate to LISTS
             .url(lists)
+            .waitForElementVisible('body', 500)
             .assert.urlEquals(lists)
             .saveScreenshot('screenshots/popcrush/smokeTest/lists.png')
  
